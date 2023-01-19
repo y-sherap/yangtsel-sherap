fe847d57ac302365081a5ad6d3d8368acb463f1e
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2023-01-19 17:40:30 -0500

    added additnl media queries

--- portfolio/src/components/portfolio/Portfolio.jsx
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -6,7 +6,7 @@ return (
     <div id='portfolio-component'>
 
         <div className="portfolio-item">
-        <a href="https://google.com" target="_blank" rel="noreferrer">
+        <a href="https://tibetan-alphabets.vercel.app/" target="_blank" rel="noreferrer">
             <img className="portfolioImage" src="https://static.dezeen.com/uploads/2022/11/sumuyakushima-tono-architecture-residential-cooperative-housing-japan_dezeen_2364_col_0-852x568.jpg" alt="graph" />
             <div className="imageOverlay">
                 <div className="imageTitle">Tibetan Alphabet</div>
--- portfolio/src/pages/info/Info.jsx
+++ portfolio/src/pages/info/Info.jsx
@@ -7,40 +7,37 @@ function Info() {
       <div id='info'>
           <div className='info-item'>Hope you had a pleasant journey navigating your way to my corner of the internet. I am a NYC based software engineer exploring alternative ways of interacting and sharing knowledge on the web~ </div>
           <div className='info-item'>I am interested in the confluence between technology, our natural and built environments, and design and how these forces shape the world around us.</div>
-        </div>
 
         <div className='info-item'>Some of my influences include:
         <ul>
-          <li><a href='https://www.versobooks.com/books/2163-extrastatecraft'>"Extra Statecraft" by Keller Easterling ↗</a></li>
+          <li><a href='https://www.versobooks.com/books/2163-extrastatecraft'>Keller Easterling ↗</a></li>
           <li><a href='https://solar.lowtechmagazine.com/'>Low-Tech Magazine ↗</a></li>
           <li><a href='http://dunneandraby.co.uk/content/projects'>Anthony Dunne and Fiona Raby ↗</a></li>
         </ul>
         </div>
+      </div>
 
       <div id="contact">
           <div className='contact-item-0'>
-          <a href="https://read.cv/ysherap">cv</a>
+          <a href="https://read.cv/ysherap" rel="noreferrer" target="_blank">cv</a>
           </div>
           <div>/</div>
           <div className='contact-item'>
-          <a href="https://read.cv/ysherap">linkedin</a>
+          <a href="https://linkedin.com/in/ysherap" rel="noreferrer" target="_blank">linkedin</a>
           </div>
           <div>/</div>
           <div className='contact-item'>
-          <a href="https://read.cv/ysherap">github</a>
+          <a href="https://github.com/y-sherap" rel="noreferrer" target="_blank">github</a>
           </div>
           <div>/</div>
           <div className='contact-item'>
-          <a href="https://read.cv/ysherap">are.na</a>
+          <a href="https://www.are.na/yangtsel-sherap" rel="noreferrer" target="_blank">are.na</a>
           </div>
           <div>/</div>
           <div className='contact-item'>
-          <a href="https://read.cv/ysherap">twitter</a>
+          <a href="https://twitter.com/realityforsale" rel="noreferrer" target="_blank">twitter</a>
           </div>
       </div>
-          
-
-
       </div>
   )
 }
