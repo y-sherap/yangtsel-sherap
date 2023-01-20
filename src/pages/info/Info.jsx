7cad024d85554f4489b33f6a337ee68e7344c0a3
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2023-01-19 19:18:31 -0500

    updated span color

--- src/pages/info/Info.jsx
+++ src/pages/info/Info.jsx
@@ -5,7 +5,7 @@ function Info() {
   return (
     <div id='info-page'>
       <div id='info'>
-          <div id='info-item-0'>Hi I'm Yangtsel 𓆟࿐</div>
+          <div id='info-item-0'>Hi I'm Yangtsel <span>𓆟࿐</span></div>
           
           <div className='info-item'>Hope you had a pleasant journey navigating your way to my corner of the internet. I am a NYC based software engineer exploring alternative ways of interacting and sharing knowledge on the web~</div> 
           <div className='info-item'>I am interested in the confluence between technology and our natural and built environments and how these forces shape the world around us.</div>
