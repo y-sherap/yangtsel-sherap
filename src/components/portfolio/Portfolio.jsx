60d2b8465c377cbaf8cb05878ee392cafafdfc62
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2023-01-25 18:25:43 -0500

    added gradient

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,11 +4,12 @@ import './portfolio.css'
 function Portfolio() {
 return (
     <div id='portfolio-component'>
+
         <div className="portfolio-item">
         <a href="https://tibetan-alphabets.vercel.app/" target="_blank" rel="noreferrer">
         <div className="imageTitle">Tibetan Alphabet</div>
         <div className="imageTitleYear">2023</div>
-            <img className="portfolioImage" src="https://t4.ftcdn.net/jpg/04/67/68/61/360_F_467686172_2201i5Pl8hflghzOBNfJwy8yoTcf8lvM.jpg" alt="graph" />
+        <div className='card-color'></div>
         </a>
         </div>
     </div>
